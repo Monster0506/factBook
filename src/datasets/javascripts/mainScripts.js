85566f0231584c6109aa8982a3be8dc399cfde35
24tr6637 <24tr6637@medinabees.org>
2022-01-10 08:52:12 -0500

    Remove IDs from table. Fix dropdown

--- src/datasets/javascripts/mainScripts.js
+++ src/datasets/javascripts/mainScripts.js
@@ -22,8 +22,7 @@ function search() {
 function huntDropdown() {
     console.log("running");
     // get the select menu and search for the selected value
-    var select = document.getElementById("hunt-select");
-    var selected = select.options[select.selectedIndex].value;
+    var select = document.getElementById("hunt-select").value.toLowerCase();
     // get the list of items and check if the selected value is in the list
     var items = document.getElementsByClassName("list-item");
     // create a number of results counter, and change the text to the number of results
@@ -33,7 +32,7 @@ function huntDropdown() {
     // check if the items list contains the search string.lower
     // use a for of loop
     for (var i of items) {
-        if (i.innerHTML.toLowerCase().includes(selected)) {
+        if (i.innerHTML.toLowerCase().includes(select)) {
             i.style.display = "block";
             counter++;
         } else {
